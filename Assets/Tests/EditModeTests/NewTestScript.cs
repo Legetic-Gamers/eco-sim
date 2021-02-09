2058c2fe7235e2b935d4ae79688a3d29f053fdf3
Alexander Huang <alexanderhuangen@live.se>
2021-02-09 23:25:19 +0100

    Added test environment for EditModeTests and PlayModeTests
    Note that an assembly has to be referenced in the different test settings!

---
+++ Assets/Tests/EditModeTests/NewTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            // Dummy test to see if it works in unity
+            Assert.AreEqual(1,1);
+            
+        }
+
+    }
+}
