98d2a25167025f833aa13ff596e43c6a0c779895
JohanAtterforsStudent <atjohan@student.chalmers.se>
2021-02-11 10:15:37 +0100

    Possibly searching for random points

--- Assets/Scripts/AnimalsV2/MoveTo.cs
+++ Assets/Scripts/AnimalsV2/MoveTo.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 using FSM;
 
 public class MoveTo : MonoBehaviour {
        
-    public Transform goal;
-       
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent agent;
     void Start () {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position; 
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        destination = agent.destination;
+        //goal = Random.insideUnitCircle * 5;
     }
+
+    void Update () {
+        if (Vector3.Distance(destination, target.position) > 1.0f){
+            destination = target.position;
+            agent.destination = destination;
+        }
+        }
 }
