2b1ac90decaf3a4fd1ce1a963661b20c4655f627
BrageLae <nicolay@student.chalmers.se>
2021-02-16 12:34:56 +0100

    Added grid and axis

--- Assets/Graph/Window_Graph.cs
+++ Assets/Graph/Window_Graph.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,20 +8,33 @@ using UnityEngine.UI;
 
 public class Window_Graph : MonoBehaviour{
 
-    private int circleSize = 11;
+    
+    //TODO make dynamic x- and y-axis, scale entire graph, make visuals connect better with logic (ie rm hard coded lengths, replace w dynamic var)
+    //TODO clean spaghetti, fix constants, clean up comments, test & explore performance
+
+    private int circleSize = 5;
     Color lineColor = new Color(1,1,1, .5f); //rgb white, 50% transparent
-    float lineWidth = 3f; // line connecting dots
+    float lineWidth = 1f; // line connecting dots
     Vector2 origo = new Vector2(0, 0);
 
     // circleSprite is for drawing circles in graph
 
     [SerializeField] private Sprite circleSprite;  
     private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
+    private RectTransform dashTemplateX;
+    private RectTransform dashTemplateY;
+    
 
     private void Awake() {
         graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+        dashTemplateX = graphContainer.Find("dashTemplateX").GetComponent<RectTransform>();
+        dashTemplateY = graphContainer.Find("dashTemplateY").GetComponent<RectTransform>();
 
-        List<int> testList = new List<int>() {5, 35, 45, 65, 54, 32, 1, 40};
+        List<int> testList = new List<int>() {0, 28, 44, 55, 64, 72, 78, 83, 88};
         ShowGraph(testList);
 
     }
@@ -42,7 +56,7 @@ public class Window_Graph : MonoBehaviour{
     }
 
     private void ShowGraph(List<int> valueList) {
-        int count = 0;
+        int count = 0; // NOTE x starts iterating on 0
         float graphHeight = graphContainer.sizeDelta.y;
         float xSize = 50f;  //distance between points in x-axis.
         float yMax = 100f; // top of graph
@@ -50,7 +64,7 @@ public class Window_Graph : MonoBehaviour{
         GameObject lastCircleGameObject = null;
 
         foreach (int value in valueList) {
-            float xPosition = (1 + count) * xSize;
+            float xPosition = (count) * xSize;
             float yPosition = (value / yMax) * graphHeight;
             GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
             
@@ -60,8 +74,35 @@ public class Window_Graph : MonoBehaviour{
                 circleGameObject.GetComponent<RectTransform>().anchoredPosition);
             }
             lastCircleGameObject = circleGameObject;
-            count++;
 
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer);
+            labelX.gameObject.SetActive(true); //template is not activated by default.
+            labelX.anchoredPosition = new Vector2 (xPosition, -7f); //TODO fix const
+            labelX.GetComponent<Text>().text = count.ToString();
+
+            RectTransform dashX = Instantiate(dashTemplateX);
+            dashX.SetParent(graphContainer);
+            dashX.gameObject.SetActive(true); //template is not activated by default.
+            dashX.anchoredPosition = new Vector2 (xPosition, -7f); //TODO fix const
+
+
+            count++;
+        }
+
+        int separatorCount = 10; //TODO fix const
+        for (int i = 0; i <= separatorCount; i++) {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer);
+            labelY.gameObject.SetActive(true); //template is not activated by default.
+            float normalizedValue = (i*1f)/separatorCount;
+            labelY.anchoredPosition = (new Vector2 (-7f, normalizedValue * graphHeight)); //TODO fix const
+            labelY.GetComponent<Text>().text = Mathf.RoundToInt(yMax * normalizedValue).ToString();
+
+            RectTransform dashY = Instantiate(dashTemplateY);
+            dashY.SetParent(graphContainer);
+            dashY.gameObject.SetActive(true); //template is not activated by default.
+            dashY.anchoredPosition = new Vector2 (-2f, normalizedValue * graphHeight); //TODO fix const
         }
     }
 
