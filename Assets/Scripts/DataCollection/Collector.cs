bfcbbdc20e7c4aef44470250ff84df2683e9d8b1
Johan <54844422+JohanAtterforsStudent@users.noreply.github.com>
2021-02-22 12:55:26 +0100

    Base for datacollection (#8)

---
+++ Assets/Scripts/DataCollection/Collector.cs
@@ -0,0 +1,48 @@
+/*
+ * Author: Johan A.
+ */
+
+using System;
+using System.Collections.Generic;
+using AnimalsV2;
+using UnityEngine;
+
+namespace DataCollection
+{
+    [Serializable]
+    public class Collector
+    {
+        // Each index is generation, value is the number of animals in that generation. 
+        public static List<Animal> totalAnimalsPerGeneration;
+        
+        // Not supposed to be AnimalController, "Type of animal"
+        public static Dictionary<AnimalController, int> numberOfEntitiesPerSpecies;
+        
+        private Collector()
+        {
+            totalAnimalsPerGeneration = new List<Animal>();
+            numberOfEntitiesPerSpecies = new Dictionary<AnimalController, int>();
+            EventSubscribe();
+        }
+        
+        private void EventSubscribe()
+        {
+            /* 
+             * Subscribe on all events
+             */
+            Debug.Log("Collector has subscribed to data events. ");
+        }
+        private void EventUnsubscribe()
+        {
+            /* 
+             Subscribe on all events
+            */
+            Debug.Log("Collector has unsubscribed to data events. ");
+        }
+
+        /*
+         * TODO Add event function definitions which are subscribed to.
+         * TDO On event write to JSON-file/s. 
+         */
+    }
+}    
