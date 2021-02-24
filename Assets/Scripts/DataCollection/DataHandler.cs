4f85fe3eaca039668daa9e4c31933561c61a351f
Johan <54844422+JohanAtterforsStudent@users.noreply.github.com>
2021-02-24 18:20:21 +0100

    Data handler (#13)
    * Collector listening to ticks
    
    * Refactor and connect graph to data collection via events in event publisher
    
    * Temporaliry disable json file writing
    
    * Collector no longer MonoBeh
    
    * Add async writing to Jsonfile named test.json

---
+++ Assets/Scripts/DataCollection/DataHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using static DataCollection.Collector;
+
+namespace DataCollection
+{
+    public class DataHandler : MonoBehaviour
+    {
+        private TickEventPublisher tickEventPublisher;
+        private Collector c;
+        //private Formatter f;
+        public Action<List<int>> Display;
+        private void Awake()
+        {
+            c = new Collector();
+            tickEventPublisher = FindObjectOfType<global::TickEventPublisher>();
+            tickEventPublisher.onCollectorUpdate += UpdateDataAndGraph;
+        }
+        
+        private void UpdateDataAndGraph()
+        {
+            c.Collect();
+            if (c.totalAnimalsAlive != null)
+            {
+                Display(c.totalAnimalsAlive);
+                Task asyncTask = Formatter.WriteToFile(c.totalAnimalsAlive);
+            }
+        }
+    }
+}
