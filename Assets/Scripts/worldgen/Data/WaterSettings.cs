5e1611d6b42f94cca21a85ef18b43af303f3749a
Carl Holmberg <holmberg.carl.j@gmail.com>
2021-02-24 13:55:50 +0100

    Added waterlevel, and a simple plane as water

---
+++ Assets/Scripts/worldgen/Data/WaterSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class WaterSettings : UpdatableData
+{
+    public bool generateWater = true;
+    [Range(0, 1)]
+    public float waterLevel;
+    public float size = 1;
+    public int gridSize = 16;
+    public float power = 3;
+    public float scale = 1;
+    public float timeScale = 1;
+}
