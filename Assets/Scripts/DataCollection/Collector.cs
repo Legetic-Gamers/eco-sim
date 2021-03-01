9a08bde96bf2aa6897f47872ed9b78c880d06a02
Johan <54844422+JohanAtterforsStudent@users.noreply.github.com>
2021-03-01 09:49:52 +0100

    Now stores data of all primitive valued traits and writes to filenames of the trait (#17)
    * Fix export files now in separete dir.
    
    * Not complete-trait collection
    
    * I hate array indexing
    
    * Add trait data collection on animal start(), formatter prints to file of name of trait (don't hate arrays anymore)

--- Assets/Scripts/DataCollection/Collector.cs
+++ Assets/Scripts/DataCollection/Collector.cs
@@ -2,28 +2,121 @@
  * Author: Johan A.
  */
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace DataCollection
 {
     public class Collector
     {
-        public List<int> totalAnimalsAlive;
+        // Sampled in time domain, number of animals in the scene. 
+        public  List<int> totalAnimalsAlive;
+        
+        //Index is generation
+        public List<int> totalAnimalsAlivePerGeneration;
+        public List<List<float>> allStatsPerGeneration;
+        
+        /*
+        These lists are contained in allStatsPerGeneration in order:
+        public List<float> sizePerGeneration;
+        public List<int> maxEnergyPerGeneration;
+        public List<int> maxHelathPerGeneration;
+        public List<int> maxHydrationPerGeneration;
+        public List<float> maxSpeedPerGeneration;
+        public List<float> edurancePerGeneration;
+        public List<int> ageLimitPerGeneration;
+        public List<float> temperatureResistPerGeneration;
+        public List<float> desirabilityResistPerGeneration;
+        public List<float> viewAnglePerGeneration;
+        public List<float> viewRadiusPerGeneration;
+        public List<float> hearingRadiusPerGeneration;
+        public List<BehaviorType> behaviorTypePerGeneration;
+        public List<Species> speciesPerGeneration;
+        BehaviorType behaviorType,
+        Species species
+        */
+        
+        /// <summary>
+        /// Constructor for a collector. Mostly initialize lists. 
+        /// </summary>
         public Collector()
         {
+            // Initialize allStatsPerGeneration as list of lists, with the first (0 th) generation set to 0 for all traits. 
+            allStatsPerGeneration = new List<List<float>>(12);
+            for (int i = 0; i < 12; i++)
+            {
+                allStatsPerGeneration.Add(new List<float>{0});
+            }
             totalAnimalsAlive = new List<int> {GameObject.FindGameObjectsWithTag("Animal").Length};
+            totalAnimalsAlivePerGeneration = new List<int> {GameObject.FindGameObjectsWithTag("Animal").Length};
         }
-
+        
+        /// <summary>
+        /// Collect is called at even time intervals by the TickEventPublisher
+        /// </summary>
         public void Collect()
         {
             AddTotalAnimals();
         }
-
+        
+        /// <summary>
+        /// Collect each animal model containing its traits. 
+        /// </summary>
+        /// <param name="am"> Animal Model containing traits.</param>
+        public void Collect(AnimalModel am)
+        {
+            int gen = am.generation;
+            
+            //TODO Choose between animals alive per time step or generation. 
+            totalAnimalsAlivePerGeneration[gen] += 1;
+            
+            // Convert the traits to a list which we can easily access. 
+            List<float> traitsInAnimal = ConvertTraitsToList(am.traits);
+            
+            // Add the traits of the animal to each global statistics list. (Extent the list if the generation increases)
+            int index = 0;
+            foreach (List<float> statList in allStatsPerGeneration)
+            {
+                if (gen > statList.Capacity) statList.AddRange(Enumerable.Repeat<float>(0, statList.Count - gen));
+                statList[gen] += traitsInAnimal[index];
+                index++;
+            }
+        }
+        
+        /// <summary>
+        /// Converts the class traits to a regular list of floats for easy access. 
+        /// </summary>
+        /// <param name="classTraits"> Type Traits; Traits of an animal model</param>
+        /// <returns> List containing the float value of each trait, see top of class for order of traits. </returns>
+        private List<float> ConvertTraitsToList(Traits classTraits)
+        {
+            // Yikes, did not find another working way
+            List<float> traits = new List<float>
+            {
+                classTraits.size,
+                classTraits.maxEnergy,
+                classTraits.maxHealth,
+                classTraits.maxHydration,
+                classTraits.maxSpeed,
+                classTraits.endurance,
+                classTraits.ageLimit,
+                classTraits.temperatureResist,
+                classTraits.desirability,
+                classTraits.viewAngle,
+                classTraits.viewRadius,
+                classTraits.hearingRadius,
+            };
+            return traits;
+        }
+        
+        /// <summary>
+        /// Check the entire scene for all objects with the "Animal" tag and add at the end of the time series list. 
+        /// </summary>
         private void AddTotalAnimals()
         {
             totalAnimalsAlive.Add(GameObject.FindGameObjectsWithTag("Animal").Length);
         }
+
     }
 }    
--- Assets/Scripts/DataCollection/DataHandler.cs
+++ Assets/Scripts/DataCollection/DataHandler.cs
@@ -1,33 +1,93 @@
+/*
+ * Author: Johan A. 
+ */
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-using static DataCollection.Collector;
+using static DataCollection.Formatter;
 
 namespace DataCollection
 {
+    /// <summary>
+    /// Must be added to the scene. 
+    /// </summary>
     public class DataHandler : MonoBehaviour
     {
+        public Action<List<int>> Display;
+        
         private TickEventPublisher tickEventPublisher;
         private Collector c;
-        //private Formatter f;
-        public Action<List<int>> Display;
+        private List<string> traitNames = new List<string>
+        {
+            "size",
+            "maxEnergy", 
+            "maxHealth",
+            "maxHydration",
+            "maxSpeed",
+            "endurance", 
+            "ageLimit", 
+            "temperatureResist", 
+            "desirability", 
+            "viewAngle", 
+            "viewRadius", 
+            "hearingRadius",
+        };
+        
+        /// <summary>
+        /// Find the Tick Event Publisher and subscribe to collecting tick
+        /// Create a new collector to handle data manipulation. 
+        /// </summary>
         private void Awake()
         {
-            c = new Collector();
             tickEventPublisher = FindObjectOfType<global::TickEventPublisher>();
             tickEventPublisher.onCollectorUpdate += UpdateDataAndGraph;
+            c = new Collector();
         }
         
+        /// <summary>
+        /// Called by Aimal Controller to log when a new animal is started in the scene.
+        /// </summary>
+        /// <param name="animalModel"> Model of animal to log the traits of. </param>
+        public void LogNewAnimal(AnimalModel animalModel)
+        {
+            c.Collect(animalModel);
+        }
+        
+        /// <summary>
+        /// Uses the Formatter to print a list in json format in /Export. name will match trait name. 
+        /// </summary>
+        /// <param name="listNumber"> List to be printed. See Traits class for order. 0 to 11 </param>
+        /// <returns></returns>
+        private async Task ExportDataToFile(int listNumber)
+        {
+            await WriteToFile(c.allStatsPerGeneration[listNumber], traitNames[listNumber]);
+        }
+
+        /// <summary>
+        /// On each tick from Event Publisher collect time series data and send to graph and write to file. 
+        /// </summary>
         private void UpdateDataAndGraph()
         {
             c.Collect();
-            if (c?.totalAnimalsAlive != null)
+            //Display(ConvertFloatListToIntList(c.allStatsPerGeneration[0]));
+            //ExportDataToFile(4);
+        }
+        
+        /// <summary>
+        /// Converts a list of floats to int by casting. Used when passing data to graph. 
+        /// </summary>
+        /// <param name="list"> List to be cast to int.</param>
+        /// <returns></returns>
+        private List<int> ConvertFloatListToIntList(List<float> list)
+        {
+            List<int> integerList = new List<int>();
+            foreach (float f in c.allStatsPerGeneration[0])
             {
-                Display(c.totalAnimalsAlive);
-                Task asyncTask = Formatter.WriteToFile(c.totalAnimalsAlive);
+                list.Add((int) f);
             }
+
+            return integerList;
         }
     }
 }
