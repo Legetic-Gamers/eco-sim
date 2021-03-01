9f8bce59472f2dd6fb09862da708e958f06ff968
Alexander Larsson Vahlberg <Legetic@hotmail.com>
2021-03-01 16:43:21 +0100

    Basic Setup of ML-Agents

---
+++ Assets/Scripts/SmartBehavior/MoveToGoalAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+
+public class MoveToGoalAgent : Agent
+{
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        base.OnActionReceived(vectorAction);
+        Debug.Log(vectorAction[0]);
+    }
+    
+    
+}
