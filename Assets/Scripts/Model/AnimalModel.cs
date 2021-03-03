9a898f9b3c064fe7f3f7e7c508a977da29e7d44e
Alexander Larsson Vahlberg <Legetic@hotmail.com>
2021-03-03 16:24:58 +0100

    Sharing of all ML related stuff

--- Assets/Scripts/Model/AnimalModel.cs
+++ Assets/Scripts/Model/AnimalModel.cs
@@ -93,7 +93,7 @@ public abstract class AnimalModel
     
     public int generation { get; set; }
     
-    public int nProducedOffspring { get; set; }
+    
     public Traits traits { get; set; }
     
     /* /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ */
