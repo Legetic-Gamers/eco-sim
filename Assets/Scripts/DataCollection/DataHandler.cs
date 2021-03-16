790236443022da5261ad822ec132e68f21e2f5b3
JohanAtterforsStudent <atjohan@student.chalmers.se>
2021-03-16 10:08:28 +0100

    Add framerate logging in graph and fix big ooof bug

--- Assets/Scripts/DataCollection/DataHandler.cs
+++ Assets/Scripts/DataCollection/DataHandler.cs
@@ -3,9 +3,12 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using static DataCollection.Formatter;
+using Debug = UnityEngine.Debug;
 
 namespace DataCollection
 {
@@ -15,6 +18,13 @@ namespace DataCollection
     public class DataHandler : MonoBehaviour
     {
         public Action<List<int>> Display;
+        
+        [SerializeField]
+        private bool ShowFrameRate;
+        public int granularity = 5; // how many frames to wait until you re-calculate the FPS
+        List<float> times;
+        private List<float> framerate;
+        private int counter;
         
         private TickEventPublisher tickEventPublisher;
         private Collector c;
@@ -39,15 +49,46 @@ namespace DataCollection
         /// Find the Tick Event Publisher and subscribe to collecting tick
         /// Create a new collector to handle data manipulation. 
         /// </summary>
-        private void Awake()
+        public void Awake()
         {
             tickEventPublisher = FindObjectOfType<global::TickEventPublisher>();
+            // Subscribe to Tick Event publisher update data and graph 
             tickEventPublisher.onCollectorUpdate += UpdateDataAndGraph;
+            
+            // Make a collector to handle data
             c = new Collector();
+            
+            // Prepare for frame rate collection
+            times = new List<float>(0);
+            framerate = new List<float>(10);
+            counter = 5;
         }
-        
+            
         /// <summary>
-        /// Called by Aimal Controller to log when a new animal is started in the scene.
+        /// Calculates the average frame rate over *granularity* frames and adds to the *framerate* list. 
+        /// </summary>
+        public void Update()
+        {
+            if (ShowFrameRate)
+            {
+                if (counter <= 0)
+                {
+                    float sum = 0;
+                    foreach (float framerate in times) sum += framerate;
+
+                    float average = sum / times.Count;
+                    float fps = 1 / average;
+                    framerate.Add(fps);
+                    counter = granularity;
+                }
+
+                times.Add(Time.deltaTime);
+                counter--;
+            }
+        }
+
+        /// <summary>
+        /// Called by Animal Controller to log when a new animal is started in the scene.
         /// </summary>
         /// <param name="animalModel"> Model of animal to log the traits of. </param>
         public void LogNewAnimal(AnimalModel animalModel)
@@ -70,13 +111,15 @@ namespace DataCollection
             await WriteToFile(c.rabbitStatsPerGenMean[listNumber], traitNames[listNumber]);
         }
 
+        // ReSharper disable Unity.PerformanceAnalysis
         /// <summary>
         /// On each tick from Event Publisher collect time series data and send to graph and write to file. 
         /// </summary>
         private void UpdateDataAndGraph()
         {
             c.Collect();
-            //Display(ConvertFloatListToIntList(c.allStatsPerGeneration[0]));
+            //Display(ConvertFloatListToIntList(c.rabbitStatsPerGenMean[0]));
+            if (ShowFrameRate) Display(ConvertFloatListToIntList(framerate));
             //ExportDataToFile(0);
         }
         
@@ -88,9 +131,9 @@ namespace DataCollection
         private List<int> ConvertFloatListToIntList(List<float> list)
         {
             List<int> integerList = new List<int>();
-            foreach (float f in list)
+            foreach (float f in list.ToList())
             {
-                list.Add((int) f);
+                integerList.Add((int) f);
             }
 
             return integerList;
