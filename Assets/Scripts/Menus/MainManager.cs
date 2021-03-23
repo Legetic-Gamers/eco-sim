fe71ac81f948d1877b727413db5f4bd8d2f30652
JohanAtterforsStudent <atjohan@student.chalmers.se>
2021-03-23 09:34:33 +0100

    Game menu HUD done

---
+++ Assets/Scripts/Menus/MainManager.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author: Johan A.
+ */
+
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+namespace Menus
+{
+    public class MainManager : MonoBehaviour
+    {
+        public void QuitSimulator()
+        {
+            Debug.Log("Quitting");
+            Application.Quit();
+        }
+    }
+}
