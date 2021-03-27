e5d8011db9f283ed80081b639eafe22560dad426
BrageLae <nicolay@student.chalmers.se>
2021-03-27 11:42:21 +0100

    Give plants age and size parameters

--- Assets/Scripts/Model/PlantModel.cs
+++ Assets/Scripts/Model/PlantModel.cs
@@ -1,14 +1,33 @@
-﻿using UnityEngine;
+﻿using ICSharpCode.NRefactory.Ast;
+using UnityEngine;
 
 namespace Model
 {
     public class PlantModel : IEdible
     {
         public bool isEaten = false;
+        public float plantSize;
+        public float plantAge;
+
+        public const float plantMaxAge = 60;
+        public const float plantMaxsize = 30;
+        
         public float GetEaten()
         {
             isEaten = true;
-            return 30f;
+            float tmp = plantSize;
+            plantSize = 0;
+            return tmp;
         }
+        
+        public PlantModel()
+        {
+            this.plantAge = 0;
+            this.plantSize = 0;
+        }
+        
+        
     }
+    
+
 }
--- Assets/Scripts/ViewController/PlantController.cs
+++ Assets/Scripts/ViewController/PlantController.cs
@@ -2,6 +2,7 @@
 using DataCollection;
 using Model;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace ViewController
 {
@@ -10,17 +11,17 @@ namespace ViewController
         private TickEventPublisher tickEventPublisher;
         
         public PlantModel plantModel;
-
+        
         private DataHandler dh;
+        
+
+        
         
         public void Start()
         {
+            tickEventPublisher = FindObjectOfType<global::TickEventPublisher>();
             plantModel = new PlantModel();
-            if (tickEventPublisher)
-            {
-                tickEventPublisher = FindObjectOfType<TickEventPublisher>();
-                tickEventPublisher.onParamTickEvent += HandleDeathStatus;    
-            }
+            EventSubscribe();
 
             dh = FindObjectOfType<DataHandler>();
             dh.LogNewPlant(plantModel);
@@ -28,15 +29,54 @@ namespace ViewController
 
         public void OnDestroy()
         {
+            EventUnSubscribe();
+        }
+
+        private void EventSubscribe()
+        {
+            if (tickEventPublisher)
+            {
+                tickEventPublisher.onParamTickEvent += HandleDeathStatus;
+                tickEventPublisher.onParamTickEvent += HandleEaten; //TODO HandleEaten should be called somewhere else.
+                tickEventPublisher.onParamTickEvent += Grow;
+            }
+        }
+
+        private void EventUnSubscribe()
+        {
             if (tickEventPublisher)
             {
                 tickEventPublisher.onParamTickEvent -= HandleDeathStatus;
+                tickEventPublisher.onParamTickEvent -= HandleEaten; //TODO this does not belong here.
+                tickEventPublisher.onParamTickEvent -= Grow;
             }
+        }
+        
+        private void Grow()
+        {
+            //TODO make not stupid increment value
+            plantModel.plantAge += 2;           
+            if (plantModel.plantSize > PlantModel.plantMaxsize) plantModel.plantSize = PlantModel.plantMaxsize;
+            else plantModel.plantSize += 2;
+
+            
+            // plant has regrown after being eaten
+            if (plantModel.isEaten && plantModel.plantSize > 15)
+            {
+                gameObject.SetActive(true);
+            }
+        }
+
+        private void HandleEaten()
+        {
+            if (!plantModel.isEaten) return;
+            gameObject.SetActive(false);
+            
         }
 
         private void HandleDeathStatus()
         {
-            if (plantModel != null && plantModel.isEaten)
+            if (plantModel != null && plantModel.plantAge > PlantModel.plantMaxAge)
             {
                 dh.LogDeadPlant(plantModel);
                 Destroy(gameObject);
