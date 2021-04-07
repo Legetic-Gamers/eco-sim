315c261950178a7d5664ace9c7c334dde36c1e1a
BrageLae <nicolay@student.chalmers.se>
2021-04-07 18:56:14 +0200

    Small plantmodel fixes

--- Assets/Scripts/Model/PlantModel.cs
+++ Assets/Scripts/Model/PlantModel.cs
@@ -8,44 +8,28 @@ namespace Model
     {
 
         public bool isEaten = false;
-<<<<<<< HEAD
         
-        public float nutritionValue { get; set; }
-
-        public PlantModel(float nutritionValue)
-        {
-            nutritionValue = nutritionValue;
-        }
-
-        public PlantModel()
-        {
-            nutritionValue = 30f;
-        }
-       
-        public float GetEaten()
-        {
-            isEaten = true;
-            return nutritionValue;
-=======
-        public float plantSize;
         public float plantAge;
 
         public const float plantMaxAge = 60;
         public const float plantMaxsize = 30;
-        
+
+
+        public float nutritionValue { get; set; }
+
         public float GetEaten()
         {
             isEaten = true;
-            float tmp = plantSize;
-            plantSize = 0;
+            float tmp = nutritionValue;
+            nutritionValue = 0;
             return tmp;
->>>>>>> dynamic-food
+
         }
         
         public PlantModel()
         {
             this.plantAge = 0;
-            this.plantSize = 0;
+            this.nutritionValue = 0;
         }
         
         
