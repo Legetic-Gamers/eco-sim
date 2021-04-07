d4ec560238b2e8318ea7d47e92b00763619bc9aa
BrageLae <nicolay@student.chalmers.se>
2021-04-07 17:26:19 +0200

    add button menu manager

--- Assets/MenuButtonManager.cs
+++ Assets/MenuButtonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuButtonManager : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class MenuButtonManager : MonoBehaviour
     {
         WindowButtonsMain.SetActive(false);
         WindowButtonsPopulation.SetActive(true);
+        
     }
 
     public void Traits()
