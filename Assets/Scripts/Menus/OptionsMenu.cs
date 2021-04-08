e38bfaf072e3302450891f903c5bd952b576879c
Johan <54844422+JohanAtterforsStudent@users.noreply.github.com>
2021-04-08 09:54:01 +0200

    Volume slider (#47)
    * Add audio mixer and volume controller
    
    * Fix main menu prefab

---
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -0,0 +1,52 @@
+/*
+ * Author: Johan A.
+ */
+
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+namespace Menus
+{
+    public class OptionsMenu : MonoBehaviour
+    {
+        private static OptionsMenu _instance;
+
+        public AudioMixer audioMixer;
+ 
+        public static OptionsMenu instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = FindObjectOfType<OptionsMenu>();
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+ 
+                return _instance;
+            }
+        }
+ 
+        void Awake() 
+        {
+            if(_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                if(this != _instance)
+                    Destroy(this.gameObject);
+            }
+        }
+        
+        public void SetVolume(float volume)
+        {
+            audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume)*20);
+        }
+    }
+}
