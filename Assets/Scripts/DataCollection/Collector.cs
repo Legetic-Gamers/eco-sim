03cf5e85279ea3c1e989144275b6065f4bfb0f72
JohanAtterforsStudent <atjohan@student.chalmers.se>
2021-04-27 15:02:24 +0200

    Fix Lists?

--- Assets/Scripts/DataCollection/Collector.cs
+++ Assets/Scripts/DataCollection/Collector.cs
@@ -13,7 +13,7 @@ namespace DataCollection
 {
     public class Collector
     {
-        private int cap = 13;
+        private int cap = 14;
         public Action onAllExtinct;
 
         public static int totalAnimalsAlive;
@@ -61,15 +61,30 @@ namespace DataCollection
         public List<int> maxHealthPerGeneration;
         public List<int> maxHydrationPerGeneration;
         public List<float> maxSpeedPerGeneration;
+        public List<int> maxReproductiveUrge
         public List<float> endurancePerGeneration;
         public List<int> ageLimitPerGeneration;
         public List<float> desirabilityResistPerGeneration;
         public List<float> viewAnglePerGeneration;
         public List<float> viewRadiusPerGeneration;
         public List<float> hearingRadiusPerGeneration;
+        
         public List<float> movementFromStartToEndPerGeneration;
         Death:
         public List<int> agePerGeneration;
+        
+        classTraits.size,
+        classTraits.maxEnergy,
+        classTraits.maxHealth,
+        classTraits.maxHydration,
+        classTraits.maxSpeed,
+        classTraits.maxReproductiveUrge,
+        classTraits.endurance,
+        classTraits.ageLimit,
+        classTraits.desirability,
+        classTraits.viewAngle,
+        classTraits.viewRadius,
+        classTraits.hearingRadius
         */
 
         /// <summary>
@@ -185,7 +200,7 @@ namespace DataCollection
             if(animalTotal.Count <= gen) animalTotal.Add(1);
             else animalTotal[gen] += 1;
 
-            for (int trait = 0; trait < 11; trait++)
+            for (int trait = 0; trait < 12; trait++)
             {
                 if(animalMean[trait].Count <= gen) animalMean[trait].Add(0);
                 if(animalVar[trait].Count <= gen) animalVar[trait].Add(0);
@@ -256,31 +271,31 @@ namespace DataCollection
             // Changes the referenced lists depending on the species of the animal. 
             (List<List<float>> animalMean, List<List<float>> animalVar, _) = GetAnimalList(am);
             
-            if (gen > animalMean[11].Count - 1)
+            if (gen > animalMean[12].Count - 1)
             {
                 //Debug.Log("generation is: " + gen + " and list length is: " + animalMean[12].Count);
                 int difference = gen + 1 - animalMean[12].Count;
                 for (int i = 0; i < difference; i++)
                 {
                     //Debug.Log("Adding: " + i);
+                    animalMean[13].Add(0);
+                    animalVar[13].Add(0);
                     animalMean[12].Add(0);
                     animalVar[12].Add(0);
-                    animalMean[11].Add(0);
-                    animalVar[11].Add(0);
                 }
             }
             
             (float meanAge, float varAge) =
-                GetNewMeanVariance(animalMean[12][gen], animalVar[12][gen], am.age, totalDeadAnimals[gen]);
+                GetNewMeanVariance(animalMean[13][gen], animalVar[13][gen], am.age, totalDeadAnimals[gen]);
             
             (float meanDist, float varDist) =
-                GetNewMeanVariance(animalMean[11][gen], animalVar[11][gen], distanceTravelled, totalDeadAnimals[gen]);
+                GetNewMeanVariance(animalMean[12][gen], animalVar[12][gen], distanceTravelled, totalDeadAnimals[gen]);
                 
-            animalMean[12][gen] = (float) Math.Round(meanAge, 1);
-            animalVar[12][gen] = (float) Math.Round(varAge, 1);
+            animalMean[13][gen] = (float) Math.Round(meanAge, 1);
+            animalVar[13][gen] = (float) Math.Round(varAge, 1);
             
-            animalMean[11][gen] = (float) Math.Round(meanDist, 1);
-            animalVar[11][gen] = (float) Math.Round(varDist, 1);
+            animalMean[12][gen] = (float) Math.Round(meanDist, 1);
+            animalVar[12][gen] = (float) Math.Round(varDist, 1);
 
             causeOfDeath[cause] = causeOfDeath[cause] += 1;
             //Debug.Log("cause of death: " + cause.ToString());
@@ -354,12 +369,13 @@ namespace DataCollection
                 classTraits.maxHealth,
                 classTraits.maxHydration,
                 classTraits.maxSpeed,
+                classTraits.maxReproductiveUrge,
                 classTraits.endurance,
                 classTraits.ageLimit,
                 classTraits.desirability,
                 classTraits.viewAngle,
                 classTraits.viewRadius,
-                classTraits.hearingRadius,
+                classTraits.hearingRadius
             };
             return traits;
         }
