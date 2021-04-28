2ebd27fad33f72331affeae81f8da54570ed95d3
Alexander Huang <alexanderhuangen@live.se>
2021-04-28 19:24:43 +0200

    small tweaks

--- Assets/Scripts/Model/WolfModel.cs
+++ Assets/Scripts/Model/WolfModel.cs
@@ -4,8 +4,8 @@ using Model;
 public class WolfModel : AnimalModel, IEdible
 {
     public WolfModel() : base(new Traits(2.35f, 250, 100, 
-                                200, 6.25f, 50f, 
-                                10, 60, 10, 
+                                200, 6.25f, 75f, 
+                                10, 75, 10, 
                                 180, 14, 10), 0)
 
     {
