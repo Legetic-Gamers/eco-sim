93a305ff02ed558898416fec5d4c0d645beaa9dc
BrageLae <nicolay@student.chalmers.se>
2021-05-07 10:52:29 +0200

    Dropdown values only change once, now detects smart rabbits

--- Assets/Scripts/Menus/ShowGraphManager.cs
+++ Assets/Scripts/Menus/ShowGraphManager.cs
@@ -12,11 +12,8 @@ namespace Menus
         public GameObject canvas;
         public GameObject graph;
         private bool isActive = false;
-
-        private bool isRabbits = false;
-        private bool isWolves = false;
-        private bool isDeer = false;
-        private bool isBears = false;
+        
+        private bool toggleOnce = true;
 
         public Action SetDropDownValues;
         
@@ -29,16 +26,13 @@ namespace Menus
             canvas.GetComponent<GraphicRaycaster>().enabled = isActive;
             graph.GetComponent<Canvas>().enabled = isActive;
             Window_Graph.IsGraphOne = isActive;
-            
 
-            if (isActive)
+            if (toggleOnce)
             {
-                if (FindObjectOfType<RabbitController>()) isRabbits = true;
-                if (FindObjectOfType<WolfController>()) isWolves = true;
-                if (FindObjectOfType<DeerController>()) isDeer = true;
-                if (FindObjectOfType<BearController>()) isBears = true;
+                toggleOnce = false;
+                SetDropDownValues();
             }
-            SetDropDownValues();
+            
         }
 
         // Use from GameMenuManager to hide graph
